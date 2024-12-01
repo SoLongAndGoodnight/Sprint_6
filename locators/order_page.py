6968d3e1ea9b7e5903f4a7591b16e034b657e45d
Elizaveta Korotkova <kavasima1994@icloud.com>
2024-12-01 19:56:03 +0200

    Fix locators

--- locators/order_page.py
+++ locators/order_page.py
@@ -14,8 +14,9 @@ class OrderDifferentButton:
     RENT_DAYS = (By.XPATH, "//div[text()='* Срок аренды']")
     RENT_DAYS_SELECT = (By.XPATH, "//div[text()='трое суток']")
     COLOR = (By.ID, "black")
-    ORDER_BUTTON = (By.XPATH, "//div[3]/button[2][text()='Заказать']")
-    CONFIRM_BUTTON = (By.XPATH, "//div[2]/button[2][text()='Да']")
+    # на странице 2 кнопки "Заказать", поэтому ищем кнопку через блок контента
+    ORDER_BUTTON = (By.XPATH, "//div[contains(@class, 'Order_Buttons__1xGrp')]//button[contains(text(), 'Заказать')]")
+    CONFIRM_BUTTON = (By.XPATH, "//button[contains(text(), 'Да')]")
     STATUS_BUTTON = (By.XPATH, '//button[text()="Посмотреть статус"]')
     UPPER_BUTTON = (By.CSS_SELECTOR, ".Button_Button__ra12g")
     BOTTOM_BUTTON = (By.CSS_SELECTOR, ".Button_Middle__1CSJM")
